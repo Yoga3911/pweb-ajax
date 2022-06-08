8d6fb5a89b4ea2ca8d574dbb1e47037982400147
Robin Hood <yoga3911@gmail.com>
2022-06-08 13:48:05 +0700

    delete, search and sort using ajax

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "test";
+$db = "ajax";
 
 $conn = mysqli_connect($servername, $username, $password, $db);
 
--- index.php
+++ index.php
@@ -16,6 +16,21 @@ require_once("db.php");
 </head>
 
 <body>
+    <!-- //!! >>> SEARCH <<< -->
+    <div class="d-flex flex-row-reverse my-3 mx-2">
+        <form class="d-flex col-4" method="POST">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_text" id="search_text">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+
+    </div>
+    <!-- //!! >>> SORT <<< -->
+    <div class="d-flex flex-row-reverse mx-2">
+        <button class="btn btn-outline-primary mx-1" type="submit" id="ZA">Z - A</button>
+        <button class="btn btn-outline-primary mx-1" type="submit" id="AZ">A - Z</button>
+        <h4>Urut berdasarkan abjad - </h4>
+    </div>
+    <!-- //!! >>> TITLE DAN BODY <<< -->
     <div class="container py-5">
         <h1 class="text-center">Data Film</h1>
         <p class="text-right"><a class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#insertModal">Tambah Film</a></p>
@@ -23,7 +38,7 @@ require_once("db.php");
 
         </div>
     </div>
-
+    <!-- //!! >>> MODAL UNTUK ADD DATA <<< -->
     <div class="modal fade" id="insertModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -53,7 +68,7 @@ require_once("db.php");
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="ajax2.js"></script>
+    <script src="ajax.js"></script>
 </body>
 
 </html>
